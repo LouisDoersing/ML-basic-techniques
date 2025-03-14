d666ae3fbed88018c1f575b55eac145e4fb5da85
loulouis02 <louisdoersing@gmail.com>
2025-03-14 18:53:18 +0100

    visualization clustering

--- clustering/KmeansSklearn.py
+++ clustering/KmeansSklearn.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn
+from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
@@ -20,16 +20,16 @@ scaledX = scaler.transform(X)
 kmeans = KMeans(n_clusters=3, n_init='auto').fit(scaledX)
 
 #make predictions
-predictionOne = [1, 12, 2, 11, 100, 3, 3, .28, 1, 5, 1, 2, 1, 1]
-predictionTwo = [4, 14, 5, 10, 12000, 3, 3, .28, 1, 5, 1, 2, 10000, 1]
+prediction_One = [1, 12, 2, 11, 100, 3, 3, .28, 1, 5, 1, 2, 1, 1]
+prediction_Two = [4, 14, 5, 10, 12000, 3, 3, .28, 1, 5, 1, 2, 10000, 1]
 
 #reshape prediction in 2D Array
-predictionOneReshape = np.array(predictionOne).reshape(1, -1)
-predictionTwoReshape = np.array(predictionTwo).reshape(1, -1)
+prediction_One_Reshape = np.array(prediction_One).reshape(1, -1)
+prediction_Two_Reshape = np.array(prediction_Two).reshape(1, -1)
 
 #normalize prediction
-predictionOne_scaled = scaler.transform(predictionOneReshape)
-predictionTwo_scaled = scaler.transform(predictionTwoReshape)
+predictionOne_scaled = scaler.transform(prediction_One_Reshape)
+predictionTwo_scaled = scaler.transform(prediction_Two_Reshape)
 
 #predict with KMeans
 predicted_cluster_one = kmeans.predict(predictionOne_scaled)
@@ -40,5 +40,20 @@ predicted_cluster_two = kmeans.predict(predictionTwo_scaled)
 print("Prediction One cluster:", predicted_cluster_one)
 print("Prediction Two cluster:", predicted_cluster_two)
 
+
+
+#Visualisierung
+# reduction from 14D to 2D with PCS for visualization
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(scaledX)
+
+print(X_pca)
+
+# create scatter Plot
+plt.figure(figsize=(8, 6))
+scatter = plt.scatter(X_pca[:, 0], X_pca[:, 1], c=kmeans.labels_, cmap='viridis', alpha=0.6)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
 
 
